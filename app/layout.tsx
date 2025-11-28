87b07ccdfd6c8d4ef6c701fc34e65d841ef8b9cb
hzqula <ilooqstrasi@gmail.com>
2025-11-29 02:21:18 +0700

    chore: remove unnecessary variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geologica, Outfit } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import localFont from "next/font/local";
